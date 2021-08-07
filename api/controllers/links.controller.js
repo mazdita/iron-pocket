94ff63f663075b990527bc4e56eae3fe6e7fed5d
Carlos del Prado <carlos.delprado@11paths.com>
2021-08-07 08:50:27 +0200

    fix: validation tip

--- api/controllers/links.controller.js
+++ api/controllers/links.controller.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const urlMetadata = require('url-metadata');
+const Link = require('../models/link.model');
 
 //** TODO: Links CRUD actions: list, detail, create, update */
 
@@ -12,10 +13,16 @@ module.exports.create = (req, res, next) => {
   // With the urlMetadata library we can request the link metadata as a promise: https://www.npmjs.com/package/url-metadata#usage
   // Please don't freak out with documentation! it's just a promise!: urlMetadata(url).then(metadata => {}).catch(error => next(error))
 
+
   const url; // TODO: get the link url from request
-  urlMetadata(url)
+  // We need validate the URL before use it at urlMetadata, we can use mongoose model validation 
+  // and select the only field/fields that we need to validate (url in this case):
+  // https://mongoosejs.com/docs/api.html#document_Document-validate
+  
+  new Link({ url }).validate('url')
+    .then(() => urlMetadata(url))
     .then(metadata => {
-      // Create link's json and store at the database
+        // Create link's json and store at the database
     })
     .catch(error => next(error))
 }
